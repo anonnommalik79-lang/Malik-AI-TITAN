983b2f1fee68b638b28fec67c04931b6c4415973
Абдумалик <anonnommalik79@gmail.com>
2026-09-06 03:39:42 +0500

    feat(shorts): add Trending screen

---
+++ app/templates/sovereign-hub-ui/app/shorts/trending/page.tsx
@@ -0,0 +1,7 @@
+import { TrendingPage } from "@/components/sovereign/shorts/ShortsDiscoveryPages"
+
+export const dynamic = "force-dynamic"
+
+export default function Page() {
+  return <TrendingPage />
+}
